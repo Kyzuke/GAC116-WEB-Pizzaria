3105bdd3f089eef0a4a13c9772ae1f8ca10fe49f
GS <silverskillskyz@gmail.com>
2025-06-15 19:17:01 -0300

    Add static files configuration and implement CSS styles for the application

--- pizzaria/pizzaria/settings.py
+++ pizzaria/pizzaria/settings.py
@@ -16,6 +16,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+STATIC_URL = '/static/'
+
+# Quando for rodar localmente
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
